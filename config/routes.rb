b78e4d87e18127b1e1c8fc98af18c4fd333b51db
rishi monohur <rishi.monohur15@gmail.com>
2023-08-03 20:13:49 +0400

    added default routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+   root to: "questions#ask"
+
   get "/answer", to: "questions#answer"
 
   get "/ask", to: "questions#ask"
